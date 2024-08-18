2addb8012f29a8312ff55bdf8dd5e1b27cf135d0
maheshsp123 <maheshsp123.work@gmail.com>
2024-08-18 13:37:55 +0530

    commit 5 simple article list in home

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { articles } from "@/data";
-import ArticleItem from "@/components/ArticleItem"
+// import ArticleItem from "@/components/ArticleItem"
+import ArticleList from "@/components/ArticleList"
 
 export default function Home() {
   return (
       <div>
         <h2>Welcome to crash course 2</h2>
-        <ArticleItem id="1" content="sample contetn" description="description sample"/>
+        <ArticleList articles={articles} />
       </div>
   );
 }
---
+++ src/components/ArticleList.js
@@ -0,0 +1,23 @@
+import ArticleItem from "./ArticleItem"
+
+const ArticleList = ({articles})=>{
+    console.log("articles count=",articles.length)
+    // console.log("articles =",articles)
+    // articles.forEach((article)=>{
+    //     console.log("article = ",article)
+        
+    // })
+    return (
+        <div>
+            <h2>Article List</h2>
+            {
+                articles.map((article)=>(
+                        // console.log "hi ar li";
+                        <ArticleItem id={article.id} content={article.content} description={article.description}/>
+                ))
+            }
+        </div>
+    )
+}
+
+export default ArticleList
